743400e6d51c874803ebe5f8156b5ac91da2568a
malik-jahangir <awan.jahangir.malik@gmail.com>
2023-05-11 20:52:46 +0500

    first commit

--- src/components/AboutUsText.js
+++ src/components/AboutUsText.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+async function fetchData() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments", {
+        cache: "no-store",
+      });
+      const data = await res.json();
+    return data;
+  }
 const AboutUsText = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/comments", {
-    cache: "no-store",
-  });
-  const data = await res.json();
-
+const data = await fetchData();
   return <p>{data[0].body}</p>;
 };
 
